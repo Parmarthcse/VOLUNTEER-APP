b5dfb4ceb0c70fede7bf9b08628299f37aa2618f
Parmarthcse <gambhirsujal22@gmail.com>
2024-03-31 00:30:50 +0530

    second commit

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,33 +1,34 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 
 const Contact = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
     const [message, setMessage] = useState("");
-const handleSendMessage = (e)=> {
-    e.preventDefault();
-}
-  return (
-    <section className='contact'>
-        <div className='container'>
-            <div className='banner'>
-                <form onSubmit={handleSendMessage}>
-                    <h2>CONTACT US</h2>
-                    <div>
-                        <input type='text' value={name} placeHolder="Your Name" onChange={(e)=> setName(e.target.value)} /> 
-                         <input type='text' value={email} placeHolder="Your Email" onChange={(e)=> setEmail(e.target.value)}/>  
-                         <input type='text'value={phone} placeHolder="Phone Number " onChange={(e)=> setPhone(e.target.value)} /> 
 
-                    </div>
-                    <textarea rows="10" value={message} placeHolder="Message " onChange={(e)=> setMessage(e.target.value)}/>
-               <button className='btn' type="submit">Send Message</button>
-                </form>
+    const handleSendMessage = (e) => {
+        e.preventDefault();
+        // Logic to handle sending message
+    };
+
+    return (
+        <section className='contact'>
+            <div className='container'>
+                <div className='banner'>
+                    <form onSubmit={handleSendMessage}>
+                        <h2>CONTACT US</h2>
+                        <div>
+                            <input type='text' value={name} placeholder="Your Name" onChange={(e) => setName(e.target.value)} /> 
+                            <input type='text' value={email} placeholder="Your Email" onChange={(e) => setEmail(e.target.value)} />  
+                            <input type='text' value={phone} placeholder="Phone Number" onChange={(e) => setPhone(e.target.value)} /> 
+                        </div>
+                        <textarea rows="10" value={message} placeholder="Message" onChange={(e) => setMessage(e.target.value)} />
+                        <button className='btn' type="submit">Send Message</button>
+                    </form>
+                </div>
             </div>
-        </div>
+        </section>
+    );
+};
 
-    </section>
-  )
-}
-
-export default Contact
+export default Contact;
